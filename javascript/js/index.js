cd0da61033d91ca45cf3f8031f286c4ad1108923
Mark Tchao <mtchao@uw.edu>
2015-10-13 16:26:46 -0700

    breaktime

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,20 @@
 /**
  * application script for index.html
  */
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    var clickMeButton = document.getElementById("click-me");
+    clickMeButton.addEventListener('click', function() {
+        alert("You clicked me!");
+    });
+
+    var closeButtons= document.querySelectorAll('.alert .close');
+    var idx;
+    var closeButton;
+    for (idx = 0; idx < closeButtons; ++idx) {
+        closeButton = closeButtons[idx];
+    }
+
+});

0d1f7c922dc7ad7b6faca7ec6a5a2fc6d8779ae7
Mark Tchao <mtchao@uw.edu>
2015-10-22 16:50:41 -0700

    finished class 10/22

--- parse/js/app.js
+++ parse/js/app.js
@@ -9,6 +9,7 @@ $(function () {
     // new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     // reference to the task list element
     var tasksList = $('#tasks-list');
@@ -18,6 +19,9 @@ $(function () {
 
     // current set of tasks
     var tasks = [];
+
+    // reference to rating element
+    var ratingElem = $('#rating');
 
     // error message
     function displayError(err) {
@@ -49,21 +53,52 @@ $(function () {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function (task) {
-            $(document.createElement('li')).text(task.get('title')).appendTo(tasksList);
+            var li = $(document.createElement('li'))
+                .text(task.get('title') + ' ')
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(tasksList)
+                .click(function () {
+                    task.set('done', !task.get('done'));
+                    task.save().then(renderTasks, displayError);
+            });
+
+            $(document.createElement('span'))
+                .raty({
+                    readOnly: true,
+                    score: (task.get('rating') || 0),
+                    hints: ['terrible','bad','good','great','fantastic']})
+                .appendTo(li);
         })
+    }
+
+    function showMessage(message) {
+        message = message || 'Hello';
+        alert(message);
     }
 
     // when the user submits the new task form...
     $('#new-task-form').submit(function(evt) {
+        // tell the browser not to do its default behavior
         evt.preventDefault();
 
+        // find the input element in this form with name attribute set to "title"
         var titleInput = $(this).find('[name="title"]');
+
+        // get current value
         var title = titleInput.val();
+
+        // create new Task and set the title
         var task = new Task();
         task.set('title', title);
-        task.save().then(fetchTasks, displayError).then(function () {
-            titleInput.val('');
-        });
+        task.set('rating', ratingElem.raty('score'));
+
+        // save new task to Parse database
+        task.save()
+            .then(fetchTasks, displayError)
+            .then(function () {
+                titleInput.val('');
+                ratingElem.raty('set', {});
+            });
 
         return false;
     });
@@ -71,5 +106,9 @@ $(function () {
     // go and fetch tasks
     fetchTasks();
 
-    // window.setInterval(fetchTasks, 3000);
+    // enable rating user interface element
+    ratingElem.raty();
+
+    // refetch tasks
+    window.setInterval(fetchTasks, 5000);
 });

6cce13ffca9bcadcd6f5c86686ea6235d2a69f8c
Mark Tchao <mtchao@uw.edu>
2015-10-27 17:17:28 -0700

    done with class 10/27

---
+++ angular/js/movies.js
@@ -0,0 +1,5 @@
+angular.module('Movies', []).controller('MoviesController', function($scope, $http) {
+    $http.get('data/movies-2014.min.json').then(function(results) {
+        $scope.movies = results.data;
+    });
+});
